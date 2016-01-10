2150b40f3e6d37930fa611bbdbf8c5e8218b7104
Matthew Corpolongo <matthew@corpolongo.com>
2016-01-10 10:12:28 -0600

    Updating mcorpolo_test to version 0.1.1

--- cookbooks/mcorpolo_test/metadata.rb
+++ cookbooks/mcorpolo_test/metadata.rb
@@ -4,4 +4,7 @@ maintainer_email 'mcorpolo@domain.com'
 license          'All rights reserved'
 description      'Installs/Configures mcorpolo_test'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+
+depends 'chef-client', '~> 4.3.2'
+depends 'starter', '~> 1.0.0'
