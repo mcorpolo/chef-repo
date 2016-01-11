f009388fa78b6f067d04e4c75de9b8531e64ee2e
Matthew Corpolongo <matthew@corpolongo.com>
2016-01-10 18:08:05 -0600

    updating default.rb

--- cookbooks/mcorpolo_test/recipes/default.rb
+++ cookbooks/mcorpolo_test/recipes/default.rb
@@ -8,3 +8,18 @@
 #
 
 include_recipe "starter"
+
+name "base"
+description "Base role applied to all nodes"
+override_attributes(
+  "chef_client" => {
+    "init_style" => "init",
+    "server_url" => "https://api.opscode.com/organizations/mcorpolo",
+    "validation_client_name" => "mcorpolo-validator"
+  }
+)
+run_list(
+  "recipe[chef-client::delete_validation]",
+  "recipe[chef-client::config]",
+  "recipe[chef-client::service]"
+)
